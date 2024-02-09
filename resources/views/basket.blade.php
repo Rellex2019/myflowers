9117f26c3e12b527f77456dc3425f4aaf687143e
Kryar <ms.zyathin@gmail.com>
2024-02-09 14:39:57 +0300

    Исправил Корзину не полностью

--- resources/views/basket.blade.php
+++ resources/views/basket.blade.php
@@ -190,14 +190,15 @@
                 <div class="bascket_word">Корзина</div>
                 <div class="main_block">
                     <img class="img_basket" src="/storage/img/5b6b549c9448b8751dc443ac5820c03e4b09c8f9r1-1541-1603v2_hq.jpg">
-
                     <div class="description">Нежный розовый букет из хриыв авы аыаы ва ыва ыв аыа выаыв азантем и пионо ываы авы авыаы ваыва ыв авы аываывыаа ыаыва ыва ыаываыаыв аываывddddddddd</div>
+                    <div class="cont_count">
                     <div class="bascket_button">
                         <button class="but">-</button>
                         <input type="number" class="count" value="100" >
                         <button class="but1">+</button>
                     </div>
                     <div class="price">6 250₽</div>
+                    </div>
                 </div>
 
                 <div id="popup" class="popup">
